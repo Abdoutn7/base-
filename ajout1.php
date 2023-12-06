2948dfbc396ce6924aadde9211bb3f2c1237c8d3
Abdoutn7 <150057305+Abdoutn7@users.noreply.github.com>
2023-12-06 11:32:56 +0100

    Add files via upload

--- ajout1.php
+++ ajout1.php
@@ -1,5 +1,4 @@
 <?php
- echo "t";
 $login=$_POST["login"];
 $passwd=$_POST["passwd"];
 $nom=$_POST["nom"];
@@ -9,15 +8,23 @@ $heure=$_POST["heure"];
 $mail=$_POST["mail"];
 $sexe=$_POST["sexe"];
 $foot=$_POST["foot"];
-$hand=$_POST["hand"]
+$hand=$_POST["hand"];
 $tennis=$_POST["tennis"];
-$pays=$_POST["pays"];
+$remarques=$_POST["remarques"];
 $pays1=$_POST["pays1"];
-$remraques=$_POST["remarques"];
+$pays=$_POST["pays"];
 require("config.php");
-$conn=mysqli_connect($server,$username,$password,$db) or die ("probleme de connexion");
-$sql="insert into users values('$login','$passwd','$nom','$prenom','$prenom','$date_naiss','$heure','$email','$sexe',$foot,$hand,$tennis,'$pays','$remarques');";
-echo $sql;
+##$conn=mysqli_connect($server,$username,$db) or die ("Probléme de connexion au serveur");
+$sql="insert into users values ('$login','$passwd','$nom','$prenom','$date_naiss','$heure','$mail','$sexe',$foot,$hand,$tennis,'$remarques','$pays');";
+if (!isset($foot)){
+    $foot="N";
+}
+if (!isset($hand)){
+    $hand="N";
+}
+if (!isset($tennis)){
+    $tennis="N";
+}
 
 
 
